402a419d5bf1518ed9db297ed20f857ae51e5176
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-08 21:20:54 +0400

    added Tasks folder

---
+++ Tasks/Task-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            if (text.Contains('a'))
+            {
+                Console.WriteLine("a herfi var");
+
+            }
+            else
+            {
+                Console.WriteLine("a herfi yoxdu");
+            }
+        }
+    }
+}
