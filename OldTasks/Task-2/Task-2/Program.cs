4161d5b7a57107b5b48745c10599f38b3a8e343f
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-09 20:51:55 +0400

    added OldTasks folder

---
+++ OldTasks/Task-2/Task-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine()) ;
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("cutdur");
+            }
+            else
+            {
+                Console.WriteLine("tekdir");
+            }
+        }
+    }
+}
